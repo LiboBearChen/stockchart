3046868ff6e69f5daeb16f71eb5a999c778ad920
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-06-16 23:52:47 -0400

    update

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactDOM} from 'react';
 import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries } from 'react-vis';
 import 'react-vis/dist/style.css';
 import { makeNormalChart, makeDiffChart } from './DataAnalyseTools';
@@ -11,37 +11,49 @@ class StockChart extends React.Component {
         super(props)
 
         this.state = {
-            chartChoice: props.chartChoice
+            chartChoice: 0
         }
     }
 
-    render() {
-        let priceArr = makeNormalChart(this.props.dataArr, this.props.chartDays);
-        let diffArr = makeDiffChart(this.props.dataArr, this.props.chartDays);
-        let chosenStock = this.props.selectedSymbolKey
-        this.setState({chartChoice: this.props.chartChoice});
-        let lines;
-        if (this.state.chartChoice === 0) {
-            lines = <LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />;
-        }
-        else if (this.state.chartChoice === 1) {
-            lines = <LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} />;
-        }
-        return (
-            <div>
-                <h1 style={{ textAlign: 'center' }}>Stock Price Chart</h1>
-                <XYPlot
-                    width={1000}
-                    height={500}>
-                    <HorizontalGridLines />
-                    <VerticalGridLines />
-                    {lines}
-                    <XAxis title="Day" />
-                    <YAxis title="Price" style={{ fill: 'red' }} />
-                </XYPlot>
-            </div>
-        )
+    handleChartChoiceChange() {
+
+    let priceArr = makeNormalChart(this.props.dataArr, this.props.chartDays);
+    let diffArr = makeDiffChart(this.props.dataArr, this.props.chartDays);
+    let chosenStock = this.props.selectedSymbolKey
+    let lines;
+
+    if (this.state.chartChoice === 0) {
+        lines = (<LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />);
     }
+    else if (this.state.chartChoice === 1) {
+        lines = (<LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} />);
+    }
+    else if (this.state.chartChoice === 2) {
+        lines = (<div><LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />
+            <LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} /></div>);
+    }
+
+    ReactDOM.render(lines, document.getElementById('line'));
+}
+
+
+render() {
+
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>Stock Price Chart</h1>
+            <XYPlot
+                width={1000}
+                height={500}>
+                <HorizontalGridLines />
+                <VerticalGridLines />
+                <div id="line"></div>
+                <XAxis title="Day" />
+                <YAxis title="Price" style={{ fill: 'red' }} />
+            </XYPlot>
+        </div>
+    )
+}
 }
 
 export default StockChart;
