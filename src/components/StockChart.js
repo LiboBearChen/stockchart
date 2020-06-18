e8db67e71b332d1b604d808233eaf57534be7832
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-06-17 23:51:58 -0400

    update

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -15,29 +15,37 @@ class StockChart extends React.Component {
         }
     }
 
-    handleChartChoiceChange() {
 
-    let priceArr = makeNormalChart(this.props.dataArr, this.props.chartDays);
-    let diffArr = makeDiffChart(this.props.dataArr, this.props.chartDays);
-    let chosenStock = this.props.selectedSymbolKey
-    let lines;
-
+renderPriceArr(priceArr, chosenStock) {
     if (this.state.chartChoice === 0) {
-        lines = (<LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />);
+      return <LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />;
+    } else {
+      return <div />;
     }
-    else if (this.state.chartChoice === 1) {
-        lines = (<LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} />);
+  }
+
+  renderDiffArr(diffArr, chosenStock) {
+    if (this.state.chartChoice === 1) {
+      return <LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} />;
+    } else {
+      return <div />;
     }
-    else if (this.state.chartChoice === 2) {
-        lines = (<div><LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />
+  }
+
+  renderBothArr(priceArr, diffArr, chosenStock) {
+    if (this.state.chartChoice === 1) {
+      return (<div><LineSeries data={priceArr[chosenStock]} style={{ stroke: 'green', strokeWidth: 3 }} />
             <LineSeries data={diffArr[chosenStock]} style={{ stroke: 'blue', strokeWidth: 3 }} /></div>);
+    } else {
+      return <div />;
     }
-
-    ReactDOM.render(lines, document.getElementById('line'));
-}
-
+  }
+ 
 
 render() {
+    let priceArr = makeNormalChart(this.props.dataArr, this.props.chartDays);
+    let diffArr = makeDiffChart(this.props.dataArr, this.props.chartDays);
+    let chosenStock = this.props.selectedSymbolKey;
 
     return (
         <div>
@@ -47,7 +55,9 @@ render() {
                 height={500}>
                 <HorizontalGridLines />
                 <VerticalGridLines />
-                <div id="line"></div>
+                {this.renderPriceArr()}
+                {this.renderDiffArr()}
+                {this.renderBothArr()}
                 <XAxis title="Day" />
                 <YAxis title="Price" style={{ fill: 'red' }} />
             </XYPlot>
