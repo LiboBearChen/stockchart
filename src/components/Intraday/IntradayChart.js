a43c1349f0aab54c4fdc969496412970aae05ea5
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-07-28 23:55:32 -0400

    update

--- src/components/Intraday/IntradayChart.js
+++ src/components/Intraday/IntradayChart.js
@@ -4,6 +4,20 @@ import 'react-vis/dist/style.css';
 
 class IntradayChart extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      stockSymbolArr: ["AAPL", "FB", "MSFT", "NFLX", "GOOGL"],
+      max: 0,
+      min: 0,
+      start:0,
+      close:0,
+      predictClose:0
+
+      
+    };
+  }
+
   render() {
 
     
@@ -11,12 +25,12 @@ class IntradayChart extends React.Component {
       <div>
         <h1 style={{ textAlign: 'center' }}>Intraday Price</h1>
         <XYPlot
-          width={1000}
+          width={1500}
           height={500}>
           <HorizontalGridLines />
           <VerticalGridLines />
 
-          <XAxis title="Day" />
+          <XAxis title="Minute" />
           <YAxis title="Price" style={{ fill: 'red' }} />
         </XYPlot>
       </div>
