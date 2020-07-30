ec8b5c63db9035c0b2af2ba7abb9050e8ad48e99
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-07-29 23:46:40 -0400

    update

--- src/components/FetchAPI.js
+++ src/components/FetchAPI.js
@@ -1,4 +1,4 @@
-export const fetchDailyData = async (stockSymbol) => {
+export const fetchDailyData = async (stockSymbol,type) => {
 
     const API_KEY = 'CX5XG0YFCZFJ41K0';
 
@@ -15,18 +15,24 @@ export const fetchDailyData = async (stockSymbol) => {
 
         for (let key in data['Time Series (Daily)']) {
             //convert string to number
-            let x=key.replace("-", "");
-            x=x.replace("-", "");
-            x=parseInt(x);
+            let x = key.replace("-", "");
+            x = x.replace("-", "");
+            x = parseInt(x);
 
             //trading suspended
-            if(!(stockSymbol==="000825" && x>20180412 && x<20180916)){
+            if (!(stockSymbol === "000825" && x > 20180412 && x < 20180916)) {
                 stockChartXValues.push(x);
-            let y=data['Time Series (Daily)'][key]['4. close'];
-            y=parseFloat(y);
-            stockChartYValues.push(y);
+                let open = data['Time Series (Daily)'][key]['1. open'];
+                let high = data['Time Series (Daily)'][key]['2. high'];
+                let low = data['Time Series (Daily)'][key]['3. low'];
+                let close = data['Time Series (Daily)'][key]['4. close'];
+                let volume = data['Time Series (Daily)'][key]['5. volume'];
+
+                
+                close = parseFloatclose);
+                stockChartYValues.push(close);
             }
-            
+
         }
 
         stockChartXValues.reverse();
