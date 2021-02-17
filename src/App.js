2ae359987b390615f28ca2d908674a705298a74e
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-16 21:08:53 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { fetchDailyData } from './components/FetchAPI';
+import { fetchDailyData } from "./components/FetchAPI";
 import StockChart from "./components/StockChart";
 import PredictChart from "./components/PredictChart";
 import SymbolPicker from "./components/SymbolPicker";
@@ -7,13 +7,11 @@ import RankingPicker from "./components/RankingPicker";
 import AnalysePicker from "./components/AnalysePicker";
 import AnalyseChart from "./components/AnalyseChart";
 import AnalyseTable from "./components/AnalyseTable/AnalyseTable";
-import { makeRelevanceArr } from './components/DataAnalyseTools';
-import {InputDays , InputSymbol} from './components/TextIput';
-import styles from './App.module.css';
-
+import { makeRelevanceArr } from "./components/DataAnalyseTools";
+import { InputDays, InputSymbol } from "./components/TextIput";
+import styles from "./App.module.css";
 
 export default class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -22,10 +20,9 @@ export default class App extends Component {
       ranking: 0,
       selectedSymbolKey: 0,
       chartChoice: 0,
-      chartDays: 100
+      chartDays: 100,
     };
   }
-
 
   //fetch all data for all stocks from API
   async getAllData() {
@@ -44,19 +41,19 @@ export default class App extends Component {
 
   handleSymbolChange = (key) => {
     this.setState({ selectedSymbolKey: key });
-  }
+  };
 
   handleDaysChange = (key) => {
     this.setState({ chartDays: key });
-  }
+  };
 
   handleChartChoiceChange = (key) => {
     this.setState({ chartChoice: key });
-  }
+  };
 
   handleRangkingChange = (key) => {
     this.setState({ ranking: key });
-  }
+  };
 
   render() {
     let tableData = makeRelevanceArr(this.state.dataArr, this.state.chartDays);
@@ -64,14 +61,24 @@ export default class App extends Component {
     return (
       <div className={styles.gridContainer}>
         <div className="styles.gridItem">
-          <StockChart dataArr={this.state.dataArr} selectedSymbolKey={this.state.selectedSymbolKey} chartChoice={this.state.chartChoice} chartDays={this.state.chartDays} />
+          <StockChart
+            dataArr={this.state.dataArr}
+            selectedSymbolKey={this.state.selectedSymbolKey}
+            chartChoice={this.state.chartChoice}
+            chartDays={this.state.chartDays}
+          />
         </div>
         <div className="styles.gridItem">
-          <SymbolPicker stockSymbolArr={this.state.stockSymbolArr} handleSymbolChange={this.handleSymbolChange} />
+          <SymbolPicker
+            stockSymbolArr={this.state.stockSymbolArr}
+            handleSymbolChange={this.handleSymbolChange}
+          />
           <h3>Stock Symbol: </h3>
-          <InputSymbol/>
+          <InputSymbol />
           <div>
-            <AnalysePicker handleChartChoiceChange={this.handleChartChoiceChange} />
+            <AnalysePicker
+              handleChartChoiceChange={this.handleChartChoiceChange}
+            />
           </div>
           <div>
             <h3>Chart Days: </h3>
@@ -79,24 +86,32 @@ export default class App extends Component {
           </div>
         </div>
         <div className="styles.gridItem">
-          <PredictChart dataArr={this.state.dataArr} selectedSymbolKey={this.state.selectedSymbolKey} />
+          <PredictChart
+            dataArr={this.state.dataArr}
+            selectedSymbolKey={this.state.selectedSymbolKey}
+          />
+        </div>
+        <div className="styles.gridItem"></div>
+        <div className="styles.gridItem">
+          <AnalyseChart
+            ranking={this.state.ranking}
+            dataArr={this.state.dataArr}
+            selectedSymbolKey={this.state.selectedSymbolKey}
+            chartDays={this.state.chartDays}
+          />
         </div>
         <div className="styles.gridItem">
-
-        </div>
-        <div className="styles.gridItem">
-          <AnalyseChart ranking={this.state.ranking} dataArr={this.state.dataArr} selectedSymbolKey={this.state.selectedSymbolKey} chartDays={this.state.chartDays} />
-        </div>
-        <div className="styles.gridItem">
-          <RankingPicker rankingArr={tableData} handleRangkingChange={this.handleRangkingChange} stockSymbolArr={this.state.stockSymbolArr} />
-          <AnalyseTable relevanceArr={tableData} stockSymbolArr={this.state.stockSymbolArr} />
+          <RankingPicker
+            rankingArr={tableData}
+            handleRangkingChange={this.handleRangkingChange}
+            stockSymbolArr={this.state.stockSymbolArr}
+          />
+          <AnalyseTable
+            relevanceArr={tableData}
+            stockSymbolArr={this.state.stockSymbolArr}
+          />
         </div>
       </div>
     );
   }
-
 }
-
-
-
-
--- src/components/AnalyseTable/AnalyseTable.js
+++ src/components/AnalyseTable/AnalyseTable.js
@@ -1,47 +1,46 @@
-import React, { Component } from 'react';
-import styles from './AnalyseTable.module.css';
+import React, { Component } from "react";
+import styles from "./AnalyseTable.module.css";
 
 class AnalyseTable extends Component {
+  renderTableData() {
+    return this.props.relevanceArr.map((relevanceObj, index) => {
+      const { symbol1, symbol2, averDiffPercen } = relevanceObj;
+      let num = index + 1;
+      return (
+        <tr key={index}>
+          <td>{num}</td>
+          <td>{this.checkSymbol(symbol1)}</td>
+          <td>{this.checkSymbol(symbol2)}</td>
+          <td>{averDiffPercen}</td>
+        </tr>
+      );
+    });
+  }
 
+  checkSymbol = (key) => {
+    return this.props.stockSymbolArr[key];
+  };
 
-    renderTableData() {
-        return this.props.relevanceArr.map((relevanceObj, index) => {
-            const { symbol1, symbol2, averDiffPercen } = relevanceObj
-            let num=index+1;
-            return (
-                <tr key={index}>
-                    <td>{num}</td>
-                    <td>{this.checkSymbol(symbol1)}</td>
-                    <td>{this.checkSymbol(symbol2)}</td>
-                    <td>{averDiffPercen}</td>
-                </tr>
-            )
-        })
-    }
-
-    checkSymbol = (key) => {
-        return this.props.stockSymbolArr[key];
-    }
-
-    render() {
-        return (
-            <div>
-                <h2 id='title' className={styles.title}>Stock Correlation</h2>
-                <table id='relevance' className={styles.relevance}>
-                    <tbody>
-                        <tr>
-                            <th>Ranking</th>
-                            <th>Stock1</th>
-                            <th>Stock2</th>
-                            <th>Index</th>
-                        </tr>
-                        {this.renderTableData()}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2 id="title" className={styles.title}>
+          Stock Correlation
+        </h2>
+        <table id="relevance" className={styles.relevance}>
+          <tbody>
+            <tr>
+              <th>Ranking</th>
+              <th>Stock1</th>
+              <th>Stock2</th>
+              <th>Index</th>
+            </tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default AnalyseTable;
-
