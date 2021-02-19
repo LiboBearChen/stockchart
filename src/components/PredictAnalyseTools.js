097a9655bfda0edc850fef646449cc8356a5fbf3
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-18 19:04:59 -0500

    update

--- src/components/PredictAnalyseTools.js
+++ src/components/PredictAnalyseTools.js
@@ -1,170 +1,164 @@
-
-
 export const arrayCutter = (longData, cutDays) => {
-    let returnedArr = [];
-    let cutNum=longData[0].length/cutDays;
-    let shortDataArr = [];
-    let dateArray = [];
-    let longDateArr=longData[0];
-    let longPriceArr=longData[1];
+  let returnedArr = [];
+  let cutNum = longData[0].length / cutDays;
+  let shortDataArr = [];
+  let dateArray = [];
+  let longDateArr = longData[0];
+  let longPriceArr = longData[1];
 
-    //at least 2 shortDatas
-    if(cutNum>1){
-        //i counts for shortData
-        for (let i = 1; i <= cutNum; i++){
-            let shortData = [];
-            let dateObj = {};
-            let cutArr=[];
+  //at least 2 shortDatas
+  if (cutNum > 1) {
+    //i counts for shortData
+    for (let i = 1; i <= cutNum; i++) {
+      let shortData = [];
+      let dateObj = {};
+      let cutArr = [];
 
-            //cut date array 
-            cutArr=longDateArr.slice(-cutDays);
-            longDateArr=longDateArr.slice(0,-cutDays);
-            shortData.push(cutArr);
-            dateObj.startDate=cutArr[0];
-            dateObj.endDate=cutArr[cutDays-1];
+      //cut date array
+      cutArr = longDateArr.slice(-cutDays);
+      longDateArr = longDateArr.slice(0, -cutDays);
+      shortData.push(cutArr);
+      dateObj.startDate = cutArr[0];
+      dateObj.endDate = cutArr[cutDays - 1];
 
-            //cut price array
-            cutArr=longPriceArr.slice(-cutDays);
-            longPriceArr=longPriceArr.slice(0,-cutDays);
-            shortData.push(cutArr);
+      //cut price array
+      cutArr = longPriceArr.slice(-cutDays);
+      longPriceArr = longPriceArr.slice(0, -cutDays);
+      shortData.push(cutArr);
 
-            shortDataArr.push(shortData);
-            dateArray.push(dateObj);
-        }
+      shortDataArr.push(shortData);
+      dateArray.push(dateObj);
     }
-    else{
-        console.log("Data is not enough for analysis!");
-    }
-    
-    //returnedArr[0] is shortDataArr, returnedArr[1] is dateArray
-    returnedArr.push(shortDataArr);
-    returnedArr.push(dateArray);
-    
-    return returnedArr;
-}
+  } else {
+    console.log("Data is not enough for analysis!");
+  }
+
+  //returnedArr[0] is shortDataArr, returnedArr[1] is dateArray
+  returnedArr.push(shortDataArr);
+  returnedArr.push(dateArray);
+
+  return returnedArr;
+};
 
 export const makePredictDataSourceArr = (longData, lastDateArr) => {
-    let returnedArr=[];
-    let dateArr=longData[0];
-    let priceArr=longData[1];
-    returnedArr=lastDateArr.map(date=>{
-        for(let i=0;i<dateArr.length;i++){
-            if(dateArr[i]==date){
-                //include the last date
-                let newDateArr = dateArr.slice(-dateArr.length+i);
-                let newPriceArr = priceArr.slice(-dateArr.length+i);
-                let obj=[];
-                obj.push(newDateArr);
-                obj.push(newPriceArr);
-                return obj;
-            }
-        }
-    });
-    return returnedArr;
-}
+  let returnedArr = [];
+  let dateArr = longData[0];
+  let priceArr = longData[1];
+  returnedArr = lastDateArr.map((date) => {
+    for (let i = 0; i < dateArr.length; i++) {
+      if (dateArr[i] == date) {
+        //include the last date
+        let newDateArr = dateArr.slice(-dateArr.length + i);
+        let newPriceArr = priceArr.slice(-dateArr.length + i);
+        let obj = [];
+        obj.push(newDateArr);
+        obj.push(newPriceArr);
+        return obj;
+      }
+    }
+  });
+  return returnedArr;
+};
 
-export const findMinDays = predictDataSourceArr => {
-    let daysArr=[];
-    let minDays;
+export const findMinDays = (predictDataSourceArr) => {
+  let daysArr = [];
+  let minDays;
 
-    daysArr=predictDataSourceArr.map(obj=>{
-        return obj[0].length;
-    });
-    daysArr.sort(function(a, b){return a - b});
-    minDays=daysArr[0];
+  daysArr = predictDataSourceArr.map((obj) => {
+    return obj[0].length;
+  });
+  daysArr.sort(function (a, b) {
+    return a - b;
+  });
+  minDays = daysArr[0];
 
-    return minDays;
-}
+  return minDays;
+};
 
 export const makeLastDateArr = (recentArrNew, expandArr) => {
-    let returnedArr=[];
+  let returnedArr = [];
 
-    let numArr=recentArrNew.map(obj=>{return obj.symbol2});
-    
-    returnedArr=numArr.map(num=>{
-        return expandArr[num][0][expandArr[num][0].length-1];
-    });
-    return returnedArr;
-}
+  let numArr = recentArrNew.map((obj) => {
+    return obj.symbol2;
+  });
+
+  returnedArr = numArr.map((num) => {
+    return expandArr[num][0][expandArr[num][0].length - 1];
+  });
+  return returnedArr;
+};
 
 export const makeRecentArr = (relevanceArr) => {
-    let returnedArr=[];
-    
-    relevanceArr.forEach(element => {
-        if(element.symbol1===0){
-            returnedArr.push(element);
-          }
-    });
+  let returnedArr = [];
 
-    return returnedArr;
-}
+  relevanceArr.forEach((element) => {
+    if (element.symbol1 === 0) {
+      returnedArr.push(element);
+    }
+  });
+
+  return returnedArr;
+};
 
 export const makeExpandArr = (recentArr, cutArr, ranking) => {
-    let returnedArr=[];
-    
-    //push the first arr
-    let firstArr=[];
-    firstArr.push(cutArr[0][1][0].concat(cutArr[0][0][0]));
-    firstArr.push(cutArr[0][1][1].concat(cutArr[0][0][1]));
-    returnedArr.push(firstArr);
+  let returnedArr = [];
 
-    //push other arr listed in the ranking
-    for(let i=0; i<ranking-1; i++){
-        let arrNum=recentArr[i].symbol2;
+  //push the first arr
+  let firstArr = [];
+  firstArr.push(cutArr[0][1][0].concat(cutArr[0][0][0]));
+  firstArr.push(cutArr[0][1][1].concat(cutArr[0][0][1]));
+  returnedArr.push(firstArr);
 
-        if(arrNum+1<cutArr[0].length){
-            let newDayArr=cutArr[0][arrNum+1][0].concat(cutArr[0][arrNum][0]);
-            let newPriceArr=cutArr[0][arrNum+1][1].concat(cutArr[0][arrNum][1]);
-            let newArr=[];
-            newArr.push(newDayArr);
-            newArr.push(newPriceArr);
-            returnedArr.push(newArr);
-        }
+  //push other arr listed in the ranking
+  for (let i = 0; i < ranking - 1; i++) {
+    let arrNum = recentArr[i].symbol2;
+
+    if (arrNum + 1 < cutArr[0].length) {
+      let newDayArr = cutArr[0][arrNum + 1][0].concat(cutArr[0][arrNum][0]);
+      let newPriceArr = cutArr[0][arrNum + 1][1].concat(cutArr[0][arrNum][1]);
+      let newArr = [];
+      newArr.push(newDayArr);
+      newArr.push(newPriceArr);
+      returnedArr.push(newArr);
     }
+  }
 
-    return returnedArr;
-}
-
-
+  return returnedArr;
+};
 
 export const makePredictPercentArr = (predictDataSourceArr, minDays) => {
+  let returnedArr = [];
 
-    let returnedArr = [];
+  for (let m = 0; m < predictDataSourceArr.length; m++) {
+    let yValues = predictDataSourceArr[m][1];
+    let tempArr = [];
+    for (let i = 1; i < minDays; i++) {
+      let per = (yValues[i] - yValues[i - 1]) / yValues[i - 1];
 
-    for (let m = 0; m < predictDataSourceArr.length; m++) {
-        let yValues = predictDataSourceArr[m][1];
-        let tempArr = [];
-        for (let i = 1; i < minDays; i++) {
-
-            
-            let per=(yValues[i]-yValues[i-1])/yValues[i-1];
-
-            tempArr.push(per);
-        }
-        returnedArr.push(tempArr);
+      tempArr.push(per);
     }
+    returnedArr.push(tempArr);
+  }
 
-    return returnedArr;
-}
+  return returnedArr;
+};
 
 export const makePredictChart = (perArr, nowPrice, chartDays) => {
+  let returnedArr = [];
 
-    let returnedArr = [];
+  for (let m = 0; m < perArr.length; m++) {
+    let tempArr = [];
+    //set nowPrice as the first base data
+    let prePrice = nowPrice;
+    for (let i = 0; i < chartDays; i++) {
+      let dataObj = {};
 
-    for (let m = 0; m < perArr.length; m++) {
-        let tempArr = [];
-        //set nowPrice as the first base data
-        let prePrice=nowPrice;
-        for (let i = 0; i < chartDays; i++) {
-            
-            let dataObj = {};
-
-            dataObj.x = i;
-            dataObj.y = prePrice*(1+perArr[m][i]);
-            prePrice=dataObj.y; 
-            tempArr.push(dataObj);
-        }
-        returnedArr.push(tempArr);
+      dataObj.x = i;
+      dataObj.y = prePrice * (1 + perArr[m][i]);
+      prePrice = dataObj.y;
+      tempArr.push(dataObj);
     }
-    return returnedArr;
-}
+    returnedArr.push(tempArr);
+  }
+  return returnedArr;
+};
--- src/components/PredictChart.js
+++ src/components/PredictChart.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries } from 'react-vis';
-import 'react-vis/dist/style.css';
-import { makeRelevanceArr } from './DataAnalyseTools';
-import { arrayCutter, makeRecentArr, makeExpandArr, makeLastDateArr , makePredictDataSourceArr, findMinDays, makePredictChart, makePredictPercentArr} from './PredictAnalyseTools';
-
+import React from "react";
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  LineSeries,
+} from "react-vis";
+import "react-vis/dist/style.css";
+import { makeRelevanceArr } from "./DataAnalyseTools";
+import {
+  arrayCutter,
+  makeRecentArr,
+  makeExpandArr,
+  makeLastDateArr,
+  makePredictDataSourceArr,
+  findMinDays,
+  makePredictChart,
+  makePredictPercentArr,
+} from "./PredictAnalyseTools";
 
 class PredictChart extends React.Component {
-
   render() {
-
     let line0, line1, line2, line3;
     let chosenStock = this.props.selectedSymbolKey;
-    if (this.props.dataArr.length>0) {
-
+    if (this.props.dataArr.length > 0) {
       //first analyse 10 days data
       let cutArr = arrayCutter(this.props.dataArr[chosenStock], 10);
       let relevanceArr = makeRelevanceArr(cutArr[0], 10);
@@ -23,37 +35,59 @@ class PredictChart extends React.Component {
       let relevanceArr1 = makeRelevanceArr(expandArr, 20);
       let recentArrNew = makeRecentArr(relevanceArr1);
 
-      let lastDateArr=makeLastDateArr(recentArrNew,expandArr);
+      let lastDateArr = makeLastDateArr(recentArrNew, expandArr);
 
-      let predictDataSourceArr=makePredictDataSourceArr(this.props.dataArr[chosenStock], lastDateArr); 
+      let predictDataSourceArr = makePredictDataSourceArr(
+        this.props.dataArr[chosenStock],
+        lastDateArr
+      );
       console.log(predictDataSourceArr);
 
-      let minDays=findMinDays(predictDataSourceArr);
+      let minDays = findMinDays(predictDataSourceArr);
       console.log(minDays);
 
-      let perArr=makePredictPercentArr(predictDataSourceArr, minDays);
+      let perArr = makePredictPercentArr(predictDataSourceArr, minDays);
       console.log(perArr);
 
-      let nowPrice=this.props.dataArr[chosenStock][1][this.props.dataArr[chosenStock][0].length-1];
+      let nowPrice = this.props.dataArr[chosenStock][1][
+        this.props.dataArr[chosenStock][0].length - 1
+      ];
 
-
-      let maxChartDays=perArr[0].length;
-;
-
+      let maxChartDays = perArr[0].length;
       let priceArr = makePredictChart(perArr, nowPrice, maxChartDays);
-      line0 = <LineSeries data={priceArr[0]} style={{ stroke: '#000000', strokeWidth: 3 }} />;
-      line1 = <LineSeries data={priceArr[1]} style={{ stroke: '#666666', strokeWidth: 3 }} />;
-      line2 = <LineSeries data={priceArr[2]} style={{ stroke: '#737373', strokeWidth: 3 }} />;
-      line3 = <LineSeries data={priceArr[3]} style={{ stroke: '#bfbfbf', strokeWidth: 3 }} />;
+      line0 = (
+        <LineSeries
+          data={priceArr[0]}
+          style={{ stroke: "#000000", strokeWidth: 3 }}
+        />
+      );
+      line1 = (
+        <LineSeries
+          data={priceArr[1]}
+          style={{ stroke: "#666666", strokeWidth: 3 }}
+        />
+      );
+      line2 = (
+        <LineSeries
+          data={priceArr[2]}
+          style={{ stroke: "#737373", strokeWidth: 3 }}
+        />
+      );
+      line3 = (
+        <LineSeries
+          data={priceArr[3]}
+          style={{ stroke: "#bfbfbf", strokeWidth: 3 }}
+        />
+      );
     }
-
 
     return (
       <div>
-        <h1 style={{ textAlign: 'center' }}>Forecast Chart (4 Most Possible Trends Satrting From The Next Trading Day)</h1>
-        <XYPlot
-          width={1000}
-          height={500}>
+        <h1 style={{ textAlign: "center" }}>
+          Forecast Chart (4 Most Possible Trends Satrting From The Next Trading
+          Day)
+        </h1>
+        <XYPlot width={1000} height={500}>
           <HorizontalGridLines />
           <VerticalGridLines />
           {line0}
@@ -61,10 +95,10 @@ class PredictChart extends React.Component {
           {line2}
           {line3}
           <XAxis title="Day" />
-          <YAxis title="Price" style={{ fill: 'red' }} />
+          <YAxis title="Price" style={{ fill: "red" }} />
         </XYPlot>
       </div>
-    )
+    );
   }
 }
 
